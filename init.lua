e4062212208a2fd315a9908a47d11d2f8dd9a2ba
MD ARSALAN <mdarsalanbgp786@gmail.com>
2024-09-22 13:03:42 +0530

    cd

--- init.lua
+++ init.lua
@@ -70,6 +70,8 @@ require("lazy").setup({
       vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
       vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
       vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+      -- Add tab to accept inline suggestions from codeium
+      vim.keymap.set('i', '<Tab>', function() return vim.fn['codeium#Accept']() end, { expr = true })
     end,
   },
   {
